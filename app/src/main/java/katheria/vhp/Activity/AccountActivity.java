76303ce6409807c3b85a0d9537e56a06f13f68fd
kunwar97 <kunwarashutoshsingh97@gmail.com>
2017-04-01 13:53:04 +0530

    removed depracated methods

--- app/src/main/java/katheria/vhp/Activity/AccountActivity.java
+++ app/src/main/java/katheria/vhp/Activity/AccountActivity.java
@@ -54,7 +54,7 @@ public class AccountActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
